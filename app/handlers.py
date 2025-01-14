a8f849ba54a9ca82056855687852d61bb1c4d011
Timofey Maksimov <tsmaksimov@edu.hse.ru>
2025-01-14 19:45:19 +0300

    Расписан смысл команд и команды

--- app/handlers.py
+++ app/handlers.py
@@ -8,5 +8,45 @@ router = Router()
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
-    await message.answer('Привет!')
-    await message.reply('Как дела?')
+    await message.answer('Привет! Чтобы пользоваться полным функционалом бота..')
+
+@router.message(Command('help'))
+async def cmd_help(message: Message):
+    await message.answer('Возможные вопросы и ответы к ним...')
+    # something
+
+@router.message(Command('vm'))
+async def cmd_vm(message: Message):
+    await message.answer('')
+    # проверка на наличие голоса пользователя в БД
+
+    # отправляем с дефолтным если что
+
+@router.message(Command('vd'))
+async def cmd_vd(message: Message):
+    await message.answer('')
+
+    # проверяем на то, что сообщение не слишком давнее + количество сообщений (флаг) парсим
+
+@router.message(Command('vmm'))
+async def cmd_vmm(message: Message):
+    await message.answer('')
+    # поменять флаг в бд
+
+@router.message(Command('stop_vmm'))
+async def cmd_stop_vmm(message: Message):
+    await message.answer('')
+    # поменять флаг в бд
+
+@router.message(Command('changevoice'))
+async def cmd_changevoice(message: Message):
+    await message.answer('')
+
+    # запросить у пользователя файл + проверка на корректность типа файла
+    # занесение голосового сообщения в базу данных
+
+@router.message(Command('del'))
+async def cmd_del(message: Message):
+    await message.answer('')
+
+    # удаляет пользователя из базы данных
